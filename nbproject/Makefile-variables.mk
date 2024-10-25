dedbc9cd9f93f1f70dc2b70dc19b87ee88a9145c
Hermes JCC <119246521+hjcc34@users.noreply.github.com>
2024-10-25 21:36:07 +0200

    Add files via upload

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HC-06_PIC16F628A_.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HC-06_PIC16F628A_.X.production.hex
